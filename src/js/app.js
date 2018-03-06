e5497a7d3c6f9f01e1759511f293dbdaa20c9de1
Alexander <ego.cogitare@gmail.com>
2018-03-06 14:43:37 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  // new Swiper('#home-slider-01', {
+  //   direction: 'horizontal',
+  //   slidesPerView: 1,
+  //   nextButton: $(this).find('.btn-next'),
+  //   prevButton: $(this).find('.btn-prev'),
+  //   paginationClickable: true,
+  //   autoplay: 5000,
+  //   loop: true,
+  //   spaceBetween: 0,
+  //   mousewheelControl: false,
+  //   speed: 1000
+  // });
+
+
+
+  // $(window).bind('resize', function() {
+  //   var width = $('body').width();
+  //
+  //   if (width < 768) {
+  //     $('.awards').appendTo($('.product-picture'));
+  //   }
+  //   if (width >= 768 && width < 1024) {
+  //     $('.awards').insertBefore($('.general-info'));
+  //   }
+  // })
+  // .trigger('resize');
+});
