e0640d5279b75a3d8536862265ddaaaaf531e7f0
Alexander <ego.cogitare@gmail.com>
2018-03-08 19:48:58 +0300

    ...

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-  // $(window).bind('resize', function() {
-  //   var width = $('body').width();
-  //
-  //   if (width < 768) {
-  //     $('.awards').appendTo($('.product-picture'));
-  //   }
-  //   if (width >= 768 && width < 1024) {
-  //     $('.awards').insertBefore($('.general-info'));
-  //   }
-  // })
-  // .trigger('resize');
-
   new Swiper('.events-slider', {
     direction: 'horizontal',
     slidesPerView: 'auto',
@@ -22,49 +10,69 @@ $(document).ready(function() {
   });
 
 
-
+  /**
+   * Date picker
+   */
   var $date = $('#date').get(0);
-  var calendar = rome(
-    $date, {
-    time: false ,
-    inputFormat: 'MM/DD/YYYY',
-    weekdayFormat: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-    dayFormat: 'D',
-    appendTo: $date.parentNode
-  })
-  .on('hide', function() {
-    $date.blur();
-  });
-  $($date).next('.fa-calendar').on('click', function() {
-    setTimeout(function() { calendar.show(); }, 100);
-  });
+  if ($date)
+  {
+    var calendar = rome(
+      $date, {
+      time: false ,
+      inputFormat: 'MM/DD/YYYY',
+      weekdayFormat: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+      dayFormat: 'D',
+      appendTo: $date.parentNode
+    })
+    .on('hide', function() {
+      $date.blur();
+    });
+    $($date).next('.fa-calendar').on('click', function() {
+      setTimeout(function() { calendar.show(); }, 100);
+    });
+  }
 
 
-
+  /**
+   * Google places autocomplete
+   */
   var autocompleteSettings = {
     //types: ['(cities)'],
     componentRestrictions: {'country': 'de'},
     language: 'de'
   };
-
   var $autocompleteFrom = $('#autocomplete-from').get(0);
-  var autocompleteFrom = new google.maps.places.Autocomplete($autocompleteFrom, autocompleteSettings)
-  .addListener('place_changed', function() {
-    $autocompleteFrom.blur();
-    console.log('Selected place', this.getPlace());
-  });
-
+  if ($autocompleteFrom)
+  {
+    var autocompleteFrom = new google.maps.places.Autocomplete($autocompleteFrom, autocompleteSettings)
+    .addListener('place_changed', function() {
+      $autocompleteFrom.blur();
+      console.log('Selected place', this.getPlace());
+    });
+  }
   var $autocompleteTo = $('#autocomplete-to').get(0);
-  var autocompleteTo = new google.maps.places.Autocomplete($autocompleteTo, autocompleteSettings)
-  .addListener('place_changed', function() {
-    $autocompleteTo.blur();
-    console.log('Selected place', this.getPlace());
-  });
+  if ($autocompleteTo)
+  {
+    var autocompleteTo = new google.maps.places.Autocomplete($autocompleteTo, autocompleteSettings)
+    .addListener('place_changed', function() {
+      $autocompleteTo.blur();
+      console.log('Selected place', this.getPlace());
+    });
+  }
 
 
+  /**
+   * Categories dropdown
+   */
   $('.filter-item.category').on('click', function() {
-    $(this).toggleClass('expanded')
-      //.find('.dropdown').slideToggle();
+    $(this).toggleClass('expanded');
   });
 
+
+  /**
+   * Close popup
+   */
+  $('.popup .close').on('click', function() {
+    $(this).closest('.popup-wrapper').removeClass('opened');
+  });
 });
